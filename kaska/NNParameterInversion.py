898e55add56f3328e8ef524a7d75e8da21c8f02d
José Gómez-Dans <jgomezdans@gmail.com>
2019-07-04 17:05:59 +0100

    Added logging to ANN inversion
    Added some logging

--- kaska/NNParameterInversion.py
+++ kaska/NNParameterInversion.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
+
+import logging
+
+from pathlib import Path
+
 import numpy as np
+
 import tensorflow as tf
+
+LOG = logging.getLogger(__name__ + ".NNParameterInversion")
+
 """Neural Network Parameter inversion for Sentinel-2"""
 
 class NNParameterInversion(object):
@@ -15,6 +24,11 @@ class NNParameterInversion(object):
         NN_file: str
             An already trained emulator that can be read in by tf.keras.
         """
+        path = Path(NNParameterInversion)
+        if not path.exists():
+            LOG.error(f"File {str(path):s} does not exist in the system")
+            raise IOError(f"File {str(path):s} does not exist in the system")
+        LOG.info(f"Using inverter file {NN_file:s}")
         self.inverse_param_model = tf.keras.models.load_model(NN_file)
         self.bands = [
             "B01",
@@ -34,6 +48,7 @@ class NNParameterInversion(object):
         # Bands to be used:
         self.b_ind = np.array([1, 2, 3, 4, 5, 6, 7, 8])
         
+        
 
     def invert_observations(self, data, date):
         """Main method to invert observations using a NN inverter. Takes a 
@@ -41,10 +56,11 @@ class NNParameterInversion(object):
         s2_observations.py, for example, where we have a `read_granule` method
         that when called with a data with observations returns the reflectance,
         mask, angles and uncertainty."""
-        print(f"Extracting {str(date):s}")
+        LOG.info(f"Extracting data for {str(date):s}...")
         # Read in the data and return a bunch of numpy arrays
         rho, mask, sza, vza, raa, rho_unc = data.read_granule(date)
         # rho will be None if there are no data available.
+        LOG.info(f"{mask.sum():d} pixels to be processed")
         if rho is not None:
             # Subset the bands that we want to use for our inversion
             rho = np.array(rho)[self.b_ind]
@@ -60,7 +76,7 @@ class NNParameterInversion(object):
                 np.ones(n_clr_pxls) * raa],
             )
             # Run the inversion, probably returns a tuple
-            print("\tInverting...")
+            LOG.info("\tInverting...")
             retval = self.inverse_param_model.predict(X.T)
             # OK, so we have the parameters. Re-arrange them on a 3D array
             # (params, ny, nx)
