8a891f7facaec481fc15b9a21aba1cd93d6259bf
José Gómez-Dans <jgomezdans@gmail.com>
2019-07-16 15:46:26 +0100

    reproject_data test works locally

--- tests/test_reproject.py
+++ tests/test_reproject.py
@@ -14,8 +14,8 @@ def test_reproject_data():
     """Test than when reprojecting a file to match another,
     the output has the same extent and size as the "target"
     one."""
-    target = "data/ESU.tif"
-    source = "data/s2_test_file.tif"
+    target = "tests/data/ESU.tif"
+    source = "tests/data/s2_test_file.tif"
     gg = reproject_data(source, target)
     g = gdal.Open(target)
     assert g.RasterXSize == gg.RasterXSize
