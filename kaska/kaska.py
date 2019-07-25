1bddd9aca8c717c961096b6da4ec3e1e28a24054
José Gómez-Dans <jgomezdans@gmail.com>
2019-07-25 13:37:46 +0100

    Using NNets provided by package
    Code now works with NNets provided by
    the package. It should still be possible to
    have user-defined NNet files too.

--- kaska/kaska.py
+++ kaska/kaska.py
@@ -162,21 +162,27 @@ class KaSKA(object):
 
 
 if __name__ == "__main__":
+    import pkgutil
+    from io import BytesIO
+
     start_date = dt.datetime(2017, 5, 1)
     end_date = dt.datetime(2017, 9, 1)
     temporal_grid_space = 5
     temporal_grid = define_temporal_grid(start_date, end_date,
                                         temporal_grid_space)
+    nn_inverter = pkgutil.get_data("kaska",
+                    "inverters/prosail_2NN.npz")
     s2_obs = Sentinel2Observations(
         "/home/ucfajlg/Data/python/KaFKA_Validation/LMU/s2_obs/",
-        "/home/ucfafyi/DATA/Prosail/prosail_2NN.npz",
+        BytesIO(nn_inverter),
         "/home/ucfajlg/Data/python/KaFKA_Validation/LMU/carto/ESU.tif",
         band_prob_threshold=20,
         chunk=None,
         time_grid=temporal_grid,
     )
     state_mask = "/home/ucfajlg/Data/python/KaFKA_Validation/LMU/carto/ESU.tif"
-    approx_inverter = "/home/ucfafyi/DATA/Prosail/Prosail_5_paras.h5"
+    approx_inverter = BytesIO(pkgutil.get_data("kaska",
+                    "inverters/Prosail_5_paras.h5"))
     kaska = KaSKA(s2_obs, temporal_grid, state_mask, approx_inverter,
                      "/tmp/")
     slai, scab, scbrown = kaska.run_retrieval()
