fab14306800f5641a9ab5c6e2633a682c31f4c48
José Gómez-Dans <jgomezdans@gmail.com>
2019-08-13 13:15:45 +0100

    Changed import to follow python3 convention

--- kaska/kaska.py
+++ kaska/kaska.py
@@ -14,7 +14,7 @@ from .s2_observations import Sentinel2Observations
 
 from .smoothn import smoothn
 
-from utils import save_output_parameters
+from .utils import save_output_parameters
 
 LOG = logging.getLogger("KaSKA")
 LOG.setLevel(logging.DEBUG)
