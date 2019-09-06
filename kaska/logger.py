0e0307ce7eeb40ba61863e262f88e3648742dfba
Jose Gomez-Dans <jgomezdans@gmail.com>
2019-09-06 13:06:55 +0100

    Logger functionality
    Just some changes I copied over from another project I'm involved with.
    Does this look sensible? Haven't tested in this code base yet, but if
    OK, then we can quickly change prints and whatever to use this.
    
    Addresses #5
    Addresses #38
    Addresses #33

--- kaska/logger.py
+++ kaska/logger.py
@@ -1,22 +1,40 @@
+#!/usr/bin/env python
+"""Generic logging functionality"""
 import os
 import logging
 
+
 from .version import __version__
 
-def create_logger(fname = None):
-    logger = logging.getLogger('KaSKA')
-    logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s -'
-                                  ' %(levelname)s - %(message)s')
-    if not logger.handlers:
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
+
+def create_logger(fname=None):
+    """Creates a python logger
+    
+    Parameters
+    ----------
+    fname : str, optional
+        If you pass a filename to this, it'll also output logging info to
+        a file.
+    
+    Returns
+    -------
+    logging
+        A log object.
+    """
+    logger = logging.getLogger(f"KaSKA-{__version__:s}->")
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s -" " %(levelname)s - %(message)s"
+    )
+
+    fh = logging.StreamHandler()
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
 
     if fname is not None:
         fh = logging.FileHandler(fname)
         fh.setLevel(logging.DEBUG)
-        fh.setFormatter(formatter)    
+        fh.setFormatter(formatter)
         logger.addHandler(fh)
     return logger
