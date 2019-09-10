64abfd5709afeb81df8a314ba499b64c75bfd745
Jose Gomez-Dans <j.gomez-dans@ucl.ac.uk>
2019-09-10 12:05:35 +0100

    Deals with tiles where not masked pixels exist
    This appears to work, I have added some condition to catch the
    situation where the mask is completely outside of the observations.

--- kaska/inference_runner.py
+++ kaska/inference_runner.py
@@ -169,11 +169,13 @@ def process_tile(the_chunk, config):
     s2_obs = copy.copy(config.s2_obs)
     s2_obs.apply_roi(ulx, uly, lrx, lry)
     chunk_mask = s2_obs.state_mask.ReadAsArray()
-    if np.sum(chunk_mask) == 0:
-        LOG.info(f"No pixels in chunk {chunk_no:s}")
+    n_unmasked_pxls = np.sum(chunk_mask)
+    if n_unmasked_pxls == 0:
+        LOG.info(f"No pixels in chunk {hex(chunk_no):s}")
         return None
     else:
         # Define KaSKA object with windowed observations.
+        LOG.info(f"Unmasked pixels in {hex(chunk_no):s}: {n_unmasked_pxls:d}")
         kaska = KaSKA(
             s2_obs,
             config.temporal_grid,
@@ -185,7 +187,6 @@ def process_tile(the_chunk, config):
         parameter_names, parameter_data = kaska.run_retrieval()
         kaska.save_s2_output(parameter_names, parameter_data)
         return parameter_names
-
 
 def kaska_runner(
     start_date,
@@ -262,6 +263,10 @@ def kaska_runner(
         A = dask_client.map(wrapper, them_chunks)
         retval = dask_client.gather(A)
 
-    parameter_names = retval[0]
-
+    try:
+        parameter_names = next(item for item in reval if item is not None)
+    except StopIteration:
+        LOG.info("No masked pixels processed! Sure mask was sensible?")
+        return []
+    LOG.info("Starting file stitching")
     return stitch_outputs(output_folder, parameter_names)
