99be931058d1cb52642a08bb59adf8619594b0bc
Jose Gomez-Dans <jgomezdans@gmail.com>
2019-09-27 18:13:25 +0100

    Removed some unnecessary code and imports
    Closes #63

--- kaska/entry.py
+++ kaska/entry.py
@@ -1,11 +1,11 @@
+#!/usr/bin/env python
 import datetime as dt
-from .kaska import KaSKA, define_temporal_grid
 from .inference_runner import kaska_runner
 from .logger import create_logger
 from .inverters import get_emulator, get_inverter
 
 
-def run_process(start_date, end_date, temporal_grid_space, s2_folder, 
+def run_process(start_date, end_date, temporal_grid_space, s2_folder,
                 state_mask, output_folder, debug=True, logfile=None,
                 dask_client=None):
 
@@ -13,13 +13,9 @@ def run_process(start_date, end_date, temporal_grid_space, s2_folder,
         logfile = f"KaSKA_{dt.datetime.now():%Y%M%d_%H%M}.log"
     LOG = create_logger(debug=debug, fname=logfile)
     LOG.info("Running KaSKA...")
-    temporal_grid = define_temporal_grid(start_date, end_date,
-                                        temporal_grid_space)
     s2_emulator = get_emulator("prosail", "Sentinel2")
     approx_inverter = get_inverter("prosail_5paras", "Sentinel2")
-    
+
     kaska_runner(start_date, end_date, temporal_grid_space, state_mask,
-                s2_folder, approx_inverter, s2_emulator, output_folder,
-                dask_client=dask_client)
-
-
+                 s2_folder, approx_inverter, s2_emulator, output_folder,
+                 dask_client=dask_client)
