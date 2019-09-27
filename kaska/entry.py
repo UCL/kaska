46c64fa379767e88443da58b09d4f745371572a4
Jose Gomez-Dans <jgomezdans@gmail.com>
2019-09-27 18:15:32 +0100

    Added block size indication again

--- kaska/entry.py
+++ kaska/entry.py
@@ -7,7 +7,7 @@ from .inverters import get_emulator, get_inverter
 
 def run_process(start_date, end_date, temporal_grid_space, s2_folder,
                 state_mask, output_folder, debug=True, logfile=None,
-                dask_client=None):
+                dask_client=None, block_size=[256, 256]):
 
     if logfile is None:
         logfile = f"KaSKA_{dt.datetime.now():%Y%M%d_%H%M}.log"
@@ -18,4 +18,4 @@ def run_process(start_date, end_date, temporal_grid_space, s2_folder,
 
     kaska_runner(start_date, end_date, temporal_grid_space, state_mask,
                  s2_folder, approx_inverter, s2_emulator, output_folder,
-                 dask_client=dask_client)
+                 dask_client=dask_client, block_size=block_size)
