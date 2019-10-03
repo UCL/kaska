f98960093d66f72576e7b22286499f1bce64cde9
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2019-10-03 16:42:42 +0100

    Re-instate entry.py, because a run_process function is needed afterall.

---
+++ kaska/entry.py
@@ -0,0 +1,31 @@
+import datetime as dt
+from .logger import create_logger
+from .inference_runner import kaska_runner
+from .inverters import get_emulator, get_inverter
+
+
+def run_process(start_date, end_date, temporal_grid_space, s2_folder,
+                state_mask, output_folder, debug=True, logfile=None,
+                dask_client=None, block_size= [256, 256]):
+
+    # Setup logger and log run info
+    if logfile is None:
+        logfile = f"KaSKA_{dt.datetime.now():%Y%M%d_%H%M}.log"
+    LOG = create_logger(debug=debug, fname=logfile)
+    LOG.info("Running KaSKA with arguments:")
+    LOG.info("start date : "+start_date.strftime('%Y%m%d'))
+    LOG.info("end date : "+end_date.strftime('%Y%m%d'))
+    LOG.info("temporal grid spacing (days): "+str(temporal_grid_space))
+    LOG.info("data folder : "+s2_folder)
+    LOG.info("state mask : "+state_mask)
+    LOG.info("output folder : "+output_folder)
+    LOG.info("debug logging : "+("ON" if debug else "OFF"))
+    LOG.info("block size : "+str(block_size[0])+"x"+str(block_size[1]))
+
+    # Prepare arguments needed to run main kaska executable
+    s2_emulator = get_emulator("prosail", "Sentinel2")
+    approx_inverter = get_inverter("prosail_5paras", "Sentinel2")
+
+    kaska_runner(start_date, end_date, temporal_grid_space, state_mask,
+                s2_folder, approx_inverter, s2_emulator, output_folder,
+                dask_client=dask_client, block_size=block_size)
