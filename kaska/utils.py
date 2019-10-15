446a527ef557b960748b1ad44026b1a636d792c7
Tim Spain <t.spain@ucl.ac.uk>
2019-10-15 11:50:24 +0100

    Linted utils.py with yapf.

--- kaska/utils.py
+++ kaska/utils.py
@@ -2,7 +2,7 @@
 import datetime as dt
 import logging
 
-from osgeo import gdal,ogr
+from osgeo import gdal, ogr
 from osgeo import osr
 
 import numpy as np
@@ -11,13 +11,16 @@ from pathlib import Path
 
 LOG = logging.getLogger(__name__)
 
+
 def define_temporal_grid(start_date, end_date, temporal_grid_space):
     """Creates a temporal grid"""
-    temporal_grid = [start_date + i*dt.timedelta(days=temporal_grid_space)
-                    for i in range(int(np.ceil(366/temporal_grid_space)))
-                    if start_date + i*dt.timedelta(days=temporal_grid_space)
-                                    <= end_date]
+    temporal_grid = [
+        start_date + i * dt.timedelta(days=temporal_grid_space)
+        for i in range(int(np.ceil(366 / temporal_grid_space)))
+        if start_date + i * dt.timedelta(days=temporal_grid_space) <= end_date
+    ]
     return temporal_grid
+
 
 def read_emulator(emulator_file="/home/ucfafyi/DATA/Prosail/prosail_2NN.npz"):
     f = np.load(str(emulator_file))
@@ -25,26 +28,27 @@ def read_emulator(emulator_file="/home/ucfafyi/DATA/Prosail/prosail_2NN.npz"):
                       Output_Layers=f.f.Output_Layers)
     return emulator
 
-def reproject_data(source_img,
-        target_img=None,
-        dstSRS=None,
-        srcSRS=None,
-        srcNodata=np.nan,
-        dstNodata=np.nan,
-        outputType=None,
-        output_format="MEM",
-        verbose=False,
-        xmin=None,
-        xmax=None,
-        ymin=None,
-        ymax=None,
-        xRes=None,
-        yRes=None,
-        xSize=None,
-        ySize=None,
-        resample=1,
-    ):
 
+def reproject_data(
+    source_img,
+    target_img=None,
+    dstSRS=None,
+    srcSRS=None,
+    srcNodata=np.nan,
+    dstNodata=np.nan,
+    outputType=None,
+    output_format="MEM",
+    verbose=False,
+    xmin=None,
+    xmax=None,
+    ymin=None,
+    ymax=None,
+    xRes=None,
+    yRes=None,
+    xSize=None,
+    ySize=None,
+    resample=1,
+):
     """
     A method that uses a source and a target images to
     reproject & clip the source image to match the extent,
@@ -52,20 +56,15 @@ def reproject_data(source_img,
 
     """
 
-    outputType = (
-        gdal.GDT_Unknown if outputType is None else outputType
-        )
+    outputType = (gdal.GDT_Unknown if outputType is None else outputType)
     if srcNodata is None:
-            try:
-                srcNodata = " ".join(
-                    [
-                        i.split("=")[1]
-                        for i in gdal.Info(source_img).split("\n")
-                        if " NoData" in i
-                    ]
-                )
-            except RuntimeError:
-                srcNodata = None
+        try:
+            srcNodata = " ".join([
+                i.split("=")[1] for i in gdal.Info(source_img).split("\n")
+                if " NoData" in i
+            ])
+        except RuntimeError:
+            srcNodata = None
     # If the output type is intenger and destination nodata is nan
     # set it to 0 to avoid warnings
     if outputType <= 5 and np.isnan(dstNodata):
@@ -79,91 +78,84 @@ def reproject_data(source_img,
             _srcSRS.ImportFromWkt(srcSRS)
     else:
         _srcSRS = None
-        
 
     if (target_img is None) & (dstSRS is None):
-            raise IOError(
-                "Projection should be specified ether from "
-                + "a file or a projection code."
-            )
+        raise IOError("Projection should be specified ether from " +
+                      "a file or a projection code.")
     elif target_img is not None:
-            try:
-                g = gdal.Open(target_img)
-            except RuntimeError:
-                g = target_img
-            geo_t = g.GetGeoTransform()
-            x_size, y_size = g.RasterXSize, g.RasterYSize
+        try:
+            g = gdal.Open(target_img)
+        except RuntimeError:
+            g = target_img
+        geo_t = g.GetGeoTransform()
+        x_size, y_size = g.RasterXSize, g.RasterYSize
 
-            if xRes is None:
-                xRes = abs(geo_t[1])
-            if yRes is None:
-                yRes = abs(geo_t[5])
+        if xRes is None:
+            xRes = abs(geo_t[1])
+        if yRes is None:
+            yRes = abs(geo_t[5])
 
-            if xSize is not None:
-                x_size = 1.0 * xSize * xRes / abs(geo_t[1])
-            if ySize is not None:
-                y_size = 1.0 * ySize * yRes / abs(geo_t[5])
+        if xSize is not None:
+            x_size = 1.0 * xSize * xRes / abs(geo_t[1])
+        if ySize is not None:
+            y_size = 1.0 * ySize * yRes / abs(geo_t[5])
 
-            xmin, xmax = (
-                min(geo_t[0], geo_t[0] + x_size * geo_t[1]),
-                max(geo_t[0], geo_t[0] + x_size * geo_t[1]),
-            )
-            ymin, ymax = (
-                min(geo_t[3], geo_t[3] + y_size * geo_t[5]),
-                max(geo_t[3], geo_t[3] + y_size * geo_t[5]),
-            )
-            dstSRS = osr.SpatialReference()
-            raster_wkt = g.GetProjection()
-            dstSRS.ImportFromWkt(raster_wkt)
-            gg = gdal.Warp(
-                "",
-                source_img,
-                format=output_format,
-                outputBounds=[xmin, ymin, xmax, ymax],
-                dstNodata=dstNodata,
-                warpOptions=["NUM_THREADS=ALL_CPUS"],
-                xRes=xRes,
-                yRes=yRes,
-                dstSRS=dstSRS,
-                outputType=outputType,
-                srcNodata=srcNodata,
-                resampleAlg=resample,
-                srcSRS=_srcSRS
-            )
+        xmin, xmax = (
+            min(geo_t[0], geo_t[0] + x_size * geo_t[1]),
+            max(geo_t[0], geo_t[0] + x_size * geo_t[1]),
+        )
+        ymin, ymax = (
+            min(geo_t[3], geo_t[3] + y_size * geo_t[5]),
+            max(geo_t[3], geo_t[3] + y_size * geo_t[5]),
+        )
+        dstSRS = osr.SpatialReference()
+        raster_wkt = g.GetProjection()
+        dstSRS.ImportFromWkt(raster_wkt)
+        gg = gdal.Warp("",
+                       source_img,
+                       format=output_format,
+                       outputBounds=[xmin, ymin, xmax, ymax],
+                       dstNodata=dstNodata,
+                       warpOptions=["NUM_THREADS=ALL_CPUS"],
+                       xRes=xRes,
+                       yRes=yRes,
+                       dstSRS=dstSRS,
+                       outputType=outputType,
+                       srcNodata=srcNodata,
+                       resampleAlg=resample,
+                       srcSRS=_srcSRS)
 
     else:
-            gg = gdal.Warp(
-                "",
-                source_img,
-                format=output_format,
-                outputBounds=[xmin, ymin, xmax, ymax],
-                xRes=xRes,
-                yRes=yRes,
-                dstSRS=dstSRS,
-                warpOptions=["NUM_THREADS=ALL_CPUS"],
-                copyMetadata=True,
-                outputType=outputType,
-                dstNodata=dstNodata,
-                srcNodata=srcNodata,
-                resampleAlg=resample,
-                srcSRS=_srcSRS
-            )
+        gg = gdal.Warp("",
+                       source_img,
+                       format=output_format,
+                       outputBounds=[xmin, ymin, xmax, ymax],
+                       xRes=xRes,
+                       yRes=yRes,
+                       dstSRS=dstSRS,
+                       warpOptions=["NUM_THREADS=ALL_CPUS"],
+                       copyMetadata=True,
+                       outputType=outputType,
+                       dstNodata=dstNodata,
+                       srcNodata=srcNodata,
+                       resampleAlg=resample,
+                       srcSRS=_srcSRS)
     if verbose:
-        LOG.debug("There are %d bands in this file, use "
-                + "g.GetRasterBand(<band>) to avoid reading the whole file."
-                % gg.RasterCount
-            )
+        LOG.debug("There are %d bands in this file, use " +
+                  "g.GetRasterBand(<band>) to avoid reading the whole file." %
+                  gg.RasterCount)
     return gg
 
 
-
-def save_output_parameters(time_grid, observations, output_folder, parameter_names,
-                           output_data, output_format="GTiff",
-                           chunk=None,
-                           options=['COMPRESS=DEFLATE',
-                                    'BIGTIFF=YES',
-                                    'PREDICTOR=1',
-                                    'TILED=YES']):
+def save_output_parameters(
+    time_grid,
+    observations,
+    output_folder,
+    parameter_names,
+    output_data,
+    output_format="GTiff",
+    chunk=None,
+    options=['COMPRESS=DEFLATE', 'BIGTIFF=YES', 'PREDICTOR=1', 'TILED=YES']):
     """Saving the output parameters as (probably all times) GeoTIFFs
     """
     output_folder = Path(output_folder)
@@ -176,27 +168,29 @@ def save_output_parameters(time_grid, observations, output_folder, parameter_nam
         for band, tstep in enumerate(time_grid):
             this_date = tstep.strftime("%Y%j")
             if chunk is None:
-                outfile = output_folder/f"s2_{param:s}_A{this_date:s}.tif"
+                outfile = output_folder / f"s2_{param:s}_A{this_date:s}.tif"
             else:
-                outfile = output_folder/f"s2_{param:s}_A{this_date:s}_{chunk:s}.tif"
+                outfile = output_folder / f"s2_{param:s}_A{this_date:s}_{chunk:s}.tif"
             if outfile.exists():
                 outfile.unlink()
             LOG.info(f"Saving file {str(outfile):s}...")
-            dst_ds = drv.Create(str(outfile), nx, ny, 1,
-                            gdal.GDT_Float32, options)
+            dst_ds = drv.Create(str(outfile), nx, ny, 1, gdal.GDT_Float32,
+                                options)
             dst_ds.SetProjection(projection)
             dst_ds.SetGeoTransform(geo_transform)
             x = dst_ds.GetRasterBand(1)
             x.WriteArray(data[band, :, :].astype(np.float32))
-            x.SetMetadata({'parameter': param,
-                            'date': time_grid[band].strftime("%Y-%m-%d"),
-                            'doy':this_date})
+            x.SetMetadata({
+                'parameter': param,
+                'date': time_grid[band].strftime("%Y-%m-%d"),
+                'doy': this_date
+            })
             dst_ds = None
             g = gdal.Open(str(outfile))
             g.BuildOverviews("average", np.power(2, np.arange(6)))
 
 
-def get_chunks(nx, ny, block_size= [256, 256]):
+def get_chunks(nx, ny, block_size=[256, 256]):
     """An iterator to provide square chunks for an image. Basically,
     you pass this function the size of an array (doesn't need to be
     square!), the block size you want the cuncks to have, and it will
@@ -248,7 +242,7 @@ def get_chunks(nx, ny, block_size= [256, 256]):
             yield this_X, this_Y, nx_valid, ny_valid, chunk_no
 
 
-def rasterise_vector(vector_f, sample_f=None,  pixel_size=20):
+def rasterise_vector(vector_f, sample_f=None, pixel_size=20):
     """Raterise a vector. Basically, pixels inside a polygon are set to 1,
     and those outside to 0. Two ways of going around this: either you 
     provide a sample raster file to define spatial extent and projection
@@ -291,14 +285,14 @@ def rasterise_vector(vector_f, sample_f=None,  pixel_size=20):
         target_dsSRS.ImportFromEPSG(4326)
         target_dsSRS = target_dsSRS.ExportToWkt()
 
-    target_ds = gdal.GetDriverByName("MEM").Create(
-                "", cols, rows, 1, gdal.GDT_Byte) 
+    target_ds = gdal.GetDriverByName("MEM").Create("", cols, rows, 1,
+                                                   gdal.GDT_Byte)
     target_ds.SetGeoTransform(geoT)
     target_ds.SetProjection(target_dsSRS)
 
-    band = target_ds.GetRasterBand(1) 
-    band.SetNoDataValue(0) 
+    band = target_ds.GetRasterBand(1)
+    band.SetNoDataValue(0)
 
     gdal.RasterizeLayer(target_ds, [1], source_layer, burn_values=[1])
-    
+
     return target_ds
