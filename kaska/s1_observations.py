431118c840918408e1717aaded110c47f88d08f5
Tim Spain <t.spain@ucl.ac.uk>
2019-10-15 11:47:20 +0100

    Linted s1_observations.py with yapf.

--- kaska/s1_observations.py
+++ kaska/s1_observations.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 """Dealing with Sentinel 1 observations"""
 
-
 import datetime as dt
 import logging
 from collections import namedtuple
@@ -19,22 +18,21 @@ LOG = logging.getLogger(__name__)
 # A SIAC data storage type
 S1data = namedtuple(
     #"S1data", "time observations uncertainty mask metadata emulator"
-    "S1data", "time VV VH theta VV_unc VH_unc"
-)
-
+    "S1data",
+    "time VV VH theta VV_unc VH_unc")
 
 layers = [
-    "sigma0_vv_norm_multi_db",
-    "sigma0_vh_norm_multi_db",
-    "localIncidenceAngle"
-    ]
+    "sigma0_vv_norm_multi_db", "sigma0_vh_norm_multi_db", "localIncidenceAngle"
+]
 
 
 def get_s1_dates(s1_file):
     """Gets the dates from a LMU processed netCDF Sentinel 1 file"""
-    times = [float(s1_file.GetRasterBand(b+1).GetMetadata()['NETCDF_DIM_time']) 
-                                for b in range(s1_file.RasterCount)]
-    times = [dt.datetime(1970,1,1) + dt.timedelta(days=x) for x in times ]
+    times = [
+        float(s1_file.GetRasterBand(b + 1).GetMetadata()['NETCDF_DIM_time'])
+        for b in range(s1_file.RasterCount)
+    ]
+    times = [dt.datetime(1970, 1, 1) + dt.timedelta(days=x) for x in times]
     LOG.info(f"Sentinel 1 First obs: {times[0].strftime('%Y-%m-%d'):s}")
     LOG.info(f"Sentinel 1 Last obs: {times[-1].strftime('%Y-%m-%d'):s}")
     return times
@@ -47,10 +45,11 @@ class Sentinel1Observations(object):
         state_mask,
         chunk=None,
         time_grid=None,
-        nc_layers = {"VV": "sigma0_vv_norm_multi_db",
-                    "VH": "sigma0_vh_norm_multi_db",
-                    "theta": "localIncidenceAngle"}
-    ):
+        nc_layers={
+            "VV": "sigma0_vv_norm_multi_db",
+            "VH": "sigma0_vh_norm_multi_db",
+            "theta": "localIncidenceAngle"
+        }):
         self.time_grid = time_grid
         self.state_mask = state_mask
         self.nc_file = Path(netCDF_file)
@@ -64,7 +63,7 @@ class Sentinel1Observations(object):
         self.lry = lry
         width = lrx - ulx
         height = uly - lry
-        
+
         self.state_mask = gdal.Translate(
             "",
             self.original_mask,
@@ -98,22 +97,24 @@ class Sentinel1Observations(object):
         # new_geoT[0] = new_geoT[0] + self.ulx*new_geoT[1]
         # new_geoT[3] = new_geoT[3] + self.uly*new_geoT[5]
         return proj, geoT.tolist()  # new_geoT.tolist()
-        
+
     def _match_to_mask(self):
         """Matches the observations to the state mask.
         """
         self.s1_data_ptr = {}
         for layer, layer_name in self.nc_layers.items():
             fname = f'NETCDF:"{self.nc_file.as_posix():s}":{layer_name:s}'
-            self.s1_data_ptr[layer] = reproject_data(fname, output_format="VRT", 
-                                                srcSRS="EPSG:4326",
-                                                target_img=self.state_mask)
+            self.s1_data_ptr[layer] = reproject_data(
+                fname,
+                output_format="VRT",
+                srcSRS="EPSG:4326",
+                target_img=self.state_mask)
         s1_dates = get_s1_dates(self.s1_data_ptr[layer])
-        self.dates = {x:(i+1) 
-                            for i, x in enumerate(s1_dates) 
-                            if ( (x >= self.time_grid[0]) and 
-                            (x <= self.time_grid[-1]))}
-
+        self.dates = {
+            x: (i + 1)
+            for i, x in enumerate(s1_dates)
+            if ((x >= self.time_grid[0]) and (x <= self.time_grid[-1]))
+        }
 
     def read_time_series(self, time_grid):
         """Reads a time series of observations. Uses the time grid to provide
@@ -132,28 +133,34 @@ class Sentinel1Observations(object):
         """
         early = time_grid[0]
         late = time_grid[-1]
-        
-        sel_dates = [k for k,v in self.dates.items()
-                           if ((k >= early) and (k < late))]
-        sel_bands = [v for k,v in self.dates.items()
-                           if ((k >= early) and (k < late))]
+
+        sel_dates = [
+            k for k, v in self.dates.items() if ((k >= early) and (k < late))
+        ]
+        sel_bands = [
+            v for k, v in self.dates.items() if ((k >= early) and (k < late))
+        ]
         obs = {}
         for ii, layer in enumerate(self.s1_data_ptr.keys()):
-                obs[layer] = np.array([self.s1_data_ptr[
-                                       layer].GetRasterBand(i).ReadAsArray()
-                                         for i in sel_bands])
-        the_obs = S1data(sel_dates, obs['VV'], obs['VH'], obs['theta'], 0.5, 0.5)
+            obs[layer] = np.array([
+                self.s1_data_ptr[layer].GetRasterBand(i).ReadAsArray()
+                for i in sel_bands
+            ])
+        the_obs = S1data(sel_dates, obs['VV'], obs['VH'], obs['theta'], 0.5,
+                         0.5)
         return the_obs
-    
+
+
 if __name__ == "__main__":
     start_date = dt.datetime(2017, 3, 1)
     end_date = dt.datetime(2017, 9, 1)
     temporal_grid_space = 5
     temporal_grid = define_temporal_grid(start_date, end_date,
-                                        temporal_grid_space)
+                                         temporal_grid_space)
     nc_file = "/data/selene/ucfajlg/ELBARA_LMU/mirror_ftp/" + \
               "141.84.52.201/S1/S1_LMU_site_2017_new.nc"
-    s1_obs = Sentinel1Observations(nc_file,
-                "/home/ucfajlg/Data/python/KaFKA_Validation/LMU/carto/ESU.tif",
-                time_grid=temporal_grid)
+    s1_obs = Sentinel1Observations(
+        nc_file,
+        "/home/ucfajlg/Data/python/KaFKA_Validation/LMU/carto/ESU.tif",
+        time_grid=temporal_grid)
     vv = s1_obs.read_time_series(temporal_grid[:5])
