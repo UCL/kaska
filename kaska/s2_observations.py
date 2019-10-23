ca55e980e6096a6c1c1a920a6900f45474de9b8c
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2019-10-23 16:13:42 +0100

    Re-arrange and document part of s2_observations.

--- kaska/s2_observations.py
+++ kaska/s2_observations.py
@@ -33,27 +33,49 @@ class Sentinel2Observations(object):
         chunk=None,
         time_grid=None
     ):
-        self.band_prob_threshold = band_prob_threshold
+        """
+        Initialise the Sentinel2Observations object.
+
+        Class members initialised in __init__ :
+        parent : the path to the parent folder containing all the data.
+        emulator :
+        original_mask : the mask to be applied to the data (tif file) originally
+                        given as input argument.
+        state_mask : the mask to be applied to the data (tif file) after possible
+                     alterations.
+        band_prob_threshold : (optional)
+        band_map : the list of available bands - hardcoded
+        chunk : the size of the chunk/tile for parallel processing (optional)
+
+        Class members initialised in _find_granules :
+        dates : list of dates to run the analysis on (data files available
+                for those dates)
+        date_data : dictionary of date-folder pairs, ordered in date
+        bands_per_observation : dictionary of date-band_map pairs, ordered in date
+        """
         parent_folder = Path(parent_folder)
         if not parent_folder.exists():
             LOG.info(f"S2 data folder: {parent_folder}")
             raise IOError("S2 data folder doesn't exist")
-        self.band_map = ['B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07',
-                        'B08', 'B8A', 'B09', 'B10','B11', 'B12']
-        # self.band_map = ['05', '08']
-
         self.parent = parent_folder
-        self.original_mask = state_mask
-        self.state_mask = state_mask
 
         f = np.load(emulator, allow_pickle=True)
         self.emulator = Two_NN(
             Hidden_Layers=f.f.Hidden_Layers, Output_Layers=f.f.Output_Layers
         )
         LOG.debug("Read emulator in")
-        LOG.debug("Searching for files....")
-        self._find_granules(self.parent, time_grid)
+
+        self.original_mask = state_mask
+        self.state_mask = state_mask
+
+        self.band_prob_threshold = band_prob_threshold
+        self.band_map = ['B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07',
+                        'B08', 'B8A', 'B09', 'B10','B11', 'B12']
+
         self.chunk = chunk
+
+        LOG.debug("Searching for files....")
+        self._find_granules(time_grid)
 
     def apply_roi(self, ulx, uly, lrx, lry):
         """Applies a region of interest (ROI) window to the state mask, which is
@@ -116,13 +138,16 @@ class Sentinel2Observations(object):
         # new_geoT[3] = new_geoT[3] + self.uly*new_geoT[5]
         return proj, geoT.tolist(), nx, ny  # new_geoT.tolist()
 
-    def _find_granules(self, parent_folder, time_grid=None):
-        """Finds granules. Currently does so by checking for
-        Feng's AOT file."""
-        # this is needed to follow symlinks
+    def _find_granules(self, time_grid=None):
+        """Finds granules within the given time grid if given.
+        Currently does so by checking for Feng's AOT file.
+        """
+
+        # Get list of files. This is needed to follow symlinks.
         test_files = [
-            x for f in parent_folder.iterdir() for x in f.rglob("**/*_aot.tif")
+            x for f in self.parent.iterdir() for x in f.rglob("**/*_aot.tif")
         ]
+        # Fish out dates from file names.
         try:
             dates = [
                     dt.datetime(*(list(map(int, f.parts[-5:-2]))))
@@ -135,10 +160,7 @@ class Sentinel2Observations(object):
                 )
                 for f in test_files
             ]
-        # Sort dates by time, as currently S2A/S2B will be part of ordering
-        
-        #test_files = sorted(test_files, key=lambda x:dates[test_files.index(x)])
-        #dates = sorted(dates)
+        # Restrict dates to time grid if given, and fill self.dates
         if time_grid is not None:
             start_date = time_grid[0]
             end_date = time_grid[-1]
@@ -148,13 +170,12 @@ class Sentinel2Observations(object):
                             if (d >= start_date) and (d <= end_date)]
         else:
             self.dates = [x.replace(hour=0,minute=0, second=0) for x in dates]
+        # Fill self.date_data dictionary with sorted dates-folders data, and self.dates
+        # with sorted dates data.
         temp_dict = dict(zip(self.dates, [f.parent for f in test_files]))
-        dates = sorted(self.dates)
+        dates = sorted(self.dates) # Sort dates by time, as currently S2A/S2B will be part of ordering
         self.date_data = {k:temp_dict[k] for k in dates}
         self.dates = dates
-        
-        #self.date_data = dict(zip(self.dates, [f.parent for f in test_files]))
-        self.bands_per_observation = {}
         LOG.info(f"Found {len(test_files):d} S2 granules")
         LOG.info(
             f"First granule: "
@@ -165,6 +186,8 @@ class Sentinel2Observations(object):
             + f"{sorted(self.dates)[-1].strftime('%Y-%m-%d'):s}"
         )
 
+        # Fill bands_per_observation dictionary with sorted dates-band_map data
+        self.bands_per_observation = {}
         for the_date in self.dates:
             self.bands_per_observation[the_date] = len(self.band_map)
 
@@ -286,6 +309,8 @@ class Sentinel2Observations(object):
 
 
 if __name__ == "__main__":
+    #import pickle
+
     time_grid = []
     today = dt.datetime(2017,1,1)
     while (today <= dt.datetime(2017, 12, 31)):
@@ -293,12 +318,17 @@ if __name__ == "__main__":
         today += dt.timedelta(days=5)
 
     s2_obs = Sentinel2Observations(
-        "/home/ucfajlg/Data/python/KaFKA_Validation/LMU/s2_obs/",
-        "/home/ucfafyi/DATA/Prosail/prosail_2NN.npz",
-        "/home/ucfajlg/Data/python/KaFKA_Validation/LMU/carto/ESU.tif",
+#        "/home/ucfajlg/Data/python/KaFKA_Validation/LMU/s2_obs/",
+#        "/home/ucfafyi/DATA/Prosail/prosail_2NN.npz",
+#        "/home/ucfajlg/Data/python/KaFKA_Validation/LMU/carto/ESU.tif",
+        "/home/ilektra/kaska/kaska/tests/data/s2_data",
+        "/home/ilektra/kaska/kaska/inverters/prosail_2NN.npz",
+        "/home/ilektra/kaska/kaska/tests/data/ESU.tif",
         band_prob_threshold=20,
         chunk=None,
         time_grid=time_grid
     )
     retval = s2_obs.read_time_series([dt.datetime(2017, 1, 1),
                                       dt.datetime(2017,12,31)])
+
+    #pickle.dumps(retval)
