5a46be39eac046677bef6ac87cfeb052f7e38a57
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2019-11-01 18:21:54 +0000

    adds warning message if gdal is no available

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 # Minimal setup.py for setuptools
+from distutils import log
 from setuptools import setup
 from setuptools.config import read_configuration
 
+try:
+    import gdal
+except ImportError:
+    log.warn("Gdal is not insalled in your sysem "
+    "we recommend to install it through conda "
+    "\n    `conda install -c conda-forge gdal`")
 
 extras = read_configuration("setup.cfg")['options']['extras_require']
 # Dev is everything
