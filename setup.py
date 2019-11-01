dd20880f7dcf9b0fd76d125084a21f126f8e115a
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2019-11-01 18:13:38 +0000

    Updates metadata and dependencies of package

--- setup.py
+++ setup.py
@@ -2,15 +2,4 @@
 
 from setuptools import setup, find_packages
 
-setup(name="kaska",
-      version="0.0.1",
-      description = "An efficient smoother",
-      url = "https://github.com/jgomezdans/kaska",
-      author = "Jose Gomez-Dans",
-      author_email = "j.gomez-dans@ucl.ac.uk",
-      license = "GNU General Public License v3",
-#      packages = find_packages(include=['kaska']),
-      packages = ['kaska'], # , 'kaska.TwoNN', 'kaska.NNParameterInversion'],
-      scripts= ['scripts/run_kaska'],
-      zip_safe=False
-      )
+setup()
