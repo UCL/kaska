58eb9323ef0519a922a93501fd465cd9d7132fa2
Matthew Gillman <m.gillman@ucl.ac.uk>
2019-11-25 11:25:30 +0000

    Fixed pycodestyle errors in kaska.py

--- kaska/kaska.py
+++ kaska/kaska.py
@@ -23,13 +23,14 @@ from .interp_fix import interp1d
 from collections import namedtuple
 
 LOG = logging.getLogger(__name__)
-            
+
+
 class KaSKA(object):
     """The main KaSKA object"""
 
     def __init__(self, observations, time_grid, state_mask, approx_inverter,
-                output_folder,
-                chunk = None):
+                 output_folder,
+                 chunk=None):
         self.time_grid = time_grid
         self.observations = observations
         self.state_mask = state_mask
@@ -56,22 +57,22 @@ class KaSKA(object):
     def _process_first_pass(self, first_passer_dict):
         """This methods takes the first pass estimates of surface parameters
         (stored as a dictionary) and assembles them into an
-        `(n_params, n_times, nx, ny)` grid. The assumption here is the 
+        `(n_params, n_times, nx, ny)` grid. The assumption here is the
         dictionary is indexed by dates (e.g. datetime objects) and that for
         each date, we have a list of parameters.
-        
+
         Parameters
         ----------
         first_passer_dict: dict
             A dictionary with first pass guesses in an irregular temporal grid
-        
+
         """
         dates = [k for k in first_passer_dict.keys()]
         n_params, nx, ny = first_passer_dict[dates[0]].shape
         param_grid = np.zeros((n_params, len(dates), nx, ny))
         for i, k in enumerate(dates):
             for j in range(n_params):
-                    param_grid[j, i, :, :] = first_passer_dict[k][j]
+                param_grid[j, i, :, :] = first_passer_dict[k][j]
         # param_grid = np.zeros((n_params, len(self.time_grid), nx, ny))
         # idx = np.argmin(np.abs(self.time_grid -
         #                 np.array(dates)[:, None]), axis=1)
@@ -91,18 +92,18 @@ class KaSKA(object):
         return dates, param_grid
 
     def run_retrieval(self):
-        """Runs the retrieval for all time-steps. It proceeds by first 
+        """Runs the retrieval for all time-steps. It proceeds by first
         inverting on a observation by observation fashion, and then performs
         a per pixel smoothing/interpolation."""
         dates, retval = self._process_first_pass(self.first_pass_inversion())
         LOG.info("Burp! Now doing temporal smoothing")
         return self._run_smoother(dates, retval)
-        #x0 = np.zeros_like(retval)
-        #for param in range(retval.shape[0]):
-        #    S = retval[param]*1
-        #    ss = smoothn(S, isrobust=True, s=1, TolZ=1e-2, axis=0)
-        #    x0[param, :, :] = ss[0]
-        #return x0
+        # x0 = np.zeros_like(retval)
+        # for param in range(retval.shape[0]):
+        #     S = retval[param]*1
+        #     ss = smoothn(S, isrobust=True, s=1, TolZ=1e-2, axis=0)
+        #     x0[param, :, :] = ss[0]
+        # return x0
 
     def _run_smoother(self, dates, parameter_block):
         """Very specific method that applies some parameter transformations
@@ -117,14 +118,15 @@ class KaSKA(object):
         cbrown = parameter_block[2, :, :, :]
         if self.save_sgl_inversion:
             save_output_parameters(dates, self.observations,
-                self.output_folder / "single_imgs", ["lai", "cab", "cbrown"],
-                           [lai, cab, cbrown], output_format="GTiff",
-                           chunk=self.chunk, fname_pattern="s2_sgl",
-                           options=['COMPRESS=DEFLATE',
-                                    'BIGTIFF=YES',
-                                    'PREDICTOR=1',
-                                    'TILED=YES'])
-               
+                                   self.output_folder / "single_imgs",
+                                   ["lai", "cab", "cbrown"],
+                                   [lai, cab, cbrown], output_format="GTiff",
+                                   chunk=self.chunk, fname_pattern="s2_sgl",
+                                   options=['COMPRESS=DEFLATE',
+                                            'BIGTIFF=YES',
+                                            'PREDICTOR=1',
+                                            'TILED=YES'])
+
         # Basically, remove weird values outside of boundaries, nans and stuff
         # Could be done simply with the previously stated data structure, as
         # this is a bit of an adhoc piece of code.
@@ -136,7 +138,7 @@ class KaSKA(object):
         cbrown[cbrown < 0] = np.nan
         # Create a mask where we have no (LAI) data
         mask = np.all(lai == 0, axis=(0))
-        
+
         # Time axes in days of year
         doys = np.array([int(x.strftime('%j')) for x in dates])
         doy_grid = np.array([int(x.strftime('%j')) for x in self.time_grid])
@@ -149,19 +151,21 @@ class KaSKA(object):
         cabi[np.isnan(cabi)] = 0
         cbrowni[np.isnan(cbrowni)] = 0
         # Smooth on observations grid
-        slai = smoothn(np.array(laii), W=2*np.array(laii), isrobust=True, s=0.05,
-                       TolZ=1e-6, axis=0)[0]
+        slai = smoothn(np.array(laii), W=2*np.array(laii), isrobust=True,
+                       s=0.05, TolZ=1e-6, axis=0)[0]
         slai[slai < 0] = 0
         scab = smoothn(np.array(cabi), W=slai, isrobust=True, s=0.5,
-                        TolZ=1e-6, axis=0)[0]
+                       TolZ=1e-6, axis=0)[0]
         scbrown = smoothn(np.array(cbrowni), W=slai, isrobust=True, s=0.5,
-                        TolZ=1e-6, axis=0)[0]
+                          TolZ=1e-6, axis=0)[0]
         # Interpolate to state grid
         laii = interp1d(doy_grid, doys, slai)
         cabi = interp1d(doy_grid, doys, scab)
-        cbrowni =  interp1d(doy_grid, doys, scbrown)
-        #return (["lai", "cab", "cbrown"], [laii, cabi, cbrowni])
-        SmootherResults = namedtuple("SmootherResults", ["temporal_grid", "slai", "scab", "scbrown"])
+        cbrowni = interp1d(doy_grid, doys, scbrown)
+        # return (["lai", "cab", "cbrown"], [laii, cabi, cbrowni])
+        SmootherResults = namedtuple("SmootherResults",
+                                     ["temporal_grid", "slai",
+                                      "scab", "scbrown"])
         return SmootherResults(self.time_grid, laii, cabi, cbrowni)
 
     def save_s2_output(self, parameter_names, output_data,
