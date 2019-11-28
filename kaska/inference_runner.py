3e28bc2815634c0cff7a7a04669271b409bdb985
Matthew Gillman <m.gillman@ucl.ac.uk>
2019-11-28 14:46:18 +0000

    Fixed some pylint errors in inference_runner.py

--- kaska/inference_runner.py
+++ kaska/inference_runner.py
@@ -25,8 +25,9 @@ Config = namedtuple(
     "Config", "s2_obs s1_obs temporal_grid state_mask inverter output_folder"
 )
 
-LOG = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
+# pylint: disable=no-else-return
 
 def stitch_outputs(output_folder, parameter_list):
     """Given a folder with some contiguous files in GeoTIFF format, this
@@ -54,21 +55,21 @@ def stitch_outputs(output_folder, parameter_list):
         A list of the stitched up files.
     """
     # Get the output folder
-    p = Path(output_folder)
+    path = Path(output_folder)
     # Loop over parameters and find all the files for all the
     # chunks and dates
     output_tiffs = {}
     for parameter in parameter_list:
-        files = sorted([fich for fich in p.glob(f"*{parameter:s}_A*_0x*.tif")])
+        # files = sorted([fich for fich in path.glob(f"*{parameter:s}_A*_0x*.tif")])
+        files = sorted(list(path.glob(f"*{parameter:s}_A*_0x*.tif")))
+        # e.g. if file is "LAI_A2019135_0xfd", fich is "A2019135"
+        # dates = sorted(list(set([fich.stem.split(parameter)[1].split("_")[1]
+        #                for fich in files])))
         dates = sorted(
-            list(
-                set(
-                    [
-                        fich.stem.split(parameter)[1].split("_")[1]
-                        for fich in files
-                    ]
-                )
-            )
+            {
+                fich.stem.split(parameter)[1].split("_")[1]
+                for fich in files
+            }
         )
         fnames = []
         # Now for each data, stitch up all the chunks for that parameter
@@ -78,21 +79,21 @@ def stitch_outputs(output_folder, parameter_list):
                 fich.as_posix() for fich in files if fich.stem.find(date) >= 0
             ]
             dst_ds = gdal.BuildVRT(
-                (p / f"{parameter:s}_{date:s}.vrt").as_posix(), sel_files
+                (path / f"{parameter:s}_{date:s}.vrt").as_posix(), sel_files
             )
             fnames.append(dst_ds.GetDescription())
             dst_ds = None
 
         # Potentially, create a multiband VRT/GTiff with all the dates?
         dst_ds = gdal.BuildVRT(
-            (p / f"{parameter:s}.vrt").as_posix(),
+            (path / f"{parameter:s}.vrt").as_posix(),
             fnames,
             options=gdal.BuildVRTOptions(separate=True),
         )
         dst_ds = None
         dst_ds = gdal.Translate(
-            (p / f"{parameter:s}.tif").as_posix(),
-            (p / f"{parameter:s}.vrt").as_posix(),
+            (path / f"{parameter:s}.tif").as_posix(),
+            (path / f"{parameter:s}.vrt").as_posix(),
             options=gdal.TranslateOptions(
                 format="GTiff",
                 creationOptions=[
@@ -108,12 +109,12 @@ def stitch_outputs(output_folder, parameter_list):
                                                     dates[band - 1][1:]})
         output_tiffs[parameter] = dst_ds.GetDescription()
         dst_ds = None
-        g = gdal.Open((p / f"{parameter:s}.tif").as_posix(), gdal.GA_Update)
-        g.BuildOverviews("average", np.power(2, np.arange(8)))
-        g = None
-        g = gdal.Translate(
-            (p / "temporary.tif").as_posix(),
-            (p / f"{parameter:s}.tif").as_posix(),
+        my_gdal = gdal.Open((path / f"{parameter:s}.tif").as_posix(), gdal.GA_Update)
+        my_gdal.BuildOverviews("average", np.power(2, np.arange(8)))
+        my_gdal = None
+        my_gdal = gdal.Translate(
+            (path / "temporary.tif").as_posix(),
+            (path / f"{parameter:s}.tif").as_posix(),
             format="GTiff",
             creationOptions=[
                 "TILED=YES",
@@ -122,13 +123,14 @@ def stitch_outputs(output_folder, parameter_list):
                 "COPY_SRC_OVERVIEWS=YES",
             ],
         )
-        shutil.move(p / "temporary.tif", (p / f"{parameter:s}.tif").as_posix())
+        shutil.move(path / "temporary.tif", (path / f"{parameter:s}.tif").as_posix())
         # Remove unneeded leftover files
         for ext in ("vrt", "ovr"):
-            for f in p.glob("*." + ext):
-                f.unlink()
+            for a_file in path.glob("*." + ext):
+                a_file.unlink()
 
-        LOG.info(f"Saved {parameter:s} file as {output_tiffs[parameter]:s}")
+        #LOG.info(f"Saved {parameter:s} file as {output_tiffs[parameter]:s}")
+        LOGGER.info("Saved %s file as %s", parameter, output_tiffs[parameter])
     return output_tiffs
 
 
@@ -150,13 +152,16 @@ def process_tile(the_chunk, config):
     list
         A list of retrieved parameters.
     """
-    # Unpack chunck object with UL pixel coordinates,
+    # Unpack chunk object with UL pixel coordinates,
     # number of pixels in tile and chunk number
-    this_X, this_Y, nx_valid, ny_valid, chunk_no = the_chunk
-    ulx = this_X
-    uly = this_Y
-    lrx = this_X + nx_valid
-    lry = this_Y + ny_valid
+    #this_x, this_y, nx_valid, ny_valid, chunk_no = the_chunk
+    #ulx = this_x
+    #uly = this_y
+    #lrx = this_x + nx_valid
+    #lry = this_y + ny_valid
+    ulx, uly, nx_valid, ny_valid, chunk_no = the_chunk
+    lrx = ulx + nx_valid
+    lry = uly + ny_valid
     # copy the observations in case we have issues with
     # references hanging around...
     # Apply the region of interest to the observations
@@ -166,12 +171,12 @@ def process_tile(the_chunk, config):
     n_unmasked_pxls = np.sum(chunk_mask)
 
     if n_unmasked_pxls == 0:
-        LOG.info(f"No pixels in chunk {hex(chunk_no):s}")
+        LOGGER.info("No pixels in chunk %s", hex(chunk_no))
         return None
     else:
         # Define KaSKA object with windowed observations.
 
-        LOG.info(f"Unmasked pixels in {hex(chunk_no):s}: {n_unmasked_pxls:d}")
+        LOGGER.info("Unmasked pixels in %s: %d", hex(chunk_no), n_unmasked_pxls)
         kaska = KaSKA(
             s2_obs,
             config.temporal_grid,
@@ -196,18 +201,18 @@ def process_tile(the_chunk, config):
 
 
 def kaska_runner(
-    start_date,
-    end_date,
-    temporal_grid_space,
-    state_mask,
-    s2_folder,
-    approx_inverter,
-    s2_emulator,
-    s1_ncfile,
-    output_folder,
-    dask_client=None,
-    block_size=[256, 256],
-    chunk=None
+        start_date,
+        end_date,
+        temporal_grid_space,
+        state_mask,
+        s2_folder,
+        approx_inverter,
+        s2_emulator,
+        s1_ncfile,
+        output_folder,
+        dask_client=None,
+        block_size=None,
+        chunk=None
 ):
     """Runs a KaSKA problem for S2 producing parameter estimates between
     `start_date` and `end_date` with a temporal spacing `temporal_grid_space`.
@@ -248,6 +253,11 @@ def kaska_runner(
     list
         A list of the processed parameters files.
     """
+
+    # Fix pylint "W0102: Dangerous default value [] as argument" warning
+    if block_size is None:
+        block_size = [256, 256]
+
     temporal_grid = define_temporal_grid(
         start_date, end_date, temporal_grid_space
     )
@@ -272,34 +282,38 @@ def kaska_runner(
         approx_inverter, output_folder
         )
     # Avoid reading mask in memory in case we fill it up
-    g = gdal.Open(state_mask)
-    ny, nx = g.RasterYSize, g.RasterXSize
+    my_gdal = gdal.Open(state_mask)
+    num_y, num_x = my_gdal.RasterYSize, my_gdal.RasterXSize
     if chunk is None:
         # Do the splitting
-        them_chunks = [the_chunk for the_chunk in get_chunks(
-            nx, ny, block_size=block_size)]
+        # them_chunks = [the_chunk for the_chunk in get_chunks(
+        #    num_x, num_y, block_size=block_size)]
+        them_chunks = list(get_chunks(num_x, num_y, block_size=block_size))
 
         wrapper = partial(process_tile, config=config)
         if dask_client:
-            A = dask_client.map(wrapper, them_chunks)
-            retval = dask_client.gather(A)
+            my_futures = dask_client.map(wrapper, them_chunks)
+            retval = dask_client.gather(my_futures)
         else:
             retval = list(map(wrapper, them_chunks))
 
         try:
             parameter_names = next(item for item in retval if item is not None)
         except StopIteration:
-            LOG.info("No masked pixels processed! Sure mask was sensible?")
+            LOGGER.info("No masked pixels processed! Sure mask was sensible?")
             return []
-        LOG.info("Starting file stitching")
+        LOGGER.info("Starting file stitching")
         return stitch_outputs(output_folder, parameter_names)
     else:
         # Do the splitting
-        LOG.info(f"Doing chunk {chunk:d}")
+        LOGGER.info("Doing chunk %d", chunk)
         the_chunk = [the_chunk
                      for the_chunk in get_chunks(
-                        nx, ny, block_size=block_size)
+                         num_x, num_y, block_size=block_size)
                      if the_chunk[-1] == chunk]
-        LOG.info("Single chunk!")
+        LOGGER.info("Single chunk!")
         wrapper(the_chunk[0])
         return None
+
+
+# pylint: enable=no-else-return
