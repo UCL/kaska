6026b3a7bdf4fd55c63d224a135f85d2b7aac41a
Matthew Gillman <m.gillman@ucl.ac.uk>
2019-12-03 09:14:18 +0000

    Fixed pylint complaints in interp_fix.py

--- kaska/interp_fix.py
+++ kaska/interp_fix.py
@@ -1,8 +1,4 @@
-import numpy as np
-from numba import jit
-
-'''
-This is a linear interpolation over 1 axis
+'''This is a linear interpolation over 1 axis
 using numpy.interp function. It uses numba
 to speed up filling gaps in the for loop.
 
@@ -12,9 +8,12 @@ ucfafyi@ucl.ac.uk
 LICENSE: GNU GENERAL PUBLIC LICENSE V3
 '''
 
+import numpy as np
+from numba import jit
 
 @jit(nopython=True)
 def interp1d(newx, oldx, oldy):
+    """Linear interpolation."""
     if oldx.shape[0] != oldy.shape[0]:
         raise ValueError('oldx and oldy must have same shape in first axis.')
     oldy_shape = oldy.shape[1:]
@@ -23,9 +22,9 @@ def interp1d(newx, oldx, oldy):
     pix_num = oldy_t.shape[0]
     newy = np.full((pix_num, new_shape), np.nan)
     for i in range(pix_num):
-        y = oldy_t[i]
-        mask = ~np.isnan(y)
+        my_y = oldy_t[i]
+        mask = ~np.isnan(my_y)
         if mask.any():
-            newy[i] = np.interp(newx, oldx[mask], y[mask])
-    newy_T = newy.transpose(1, 0).reshape((new_shape,) + oldy_shape)
-    return newy_T
+            newy[i] = np.interp(newx, oldx[mask], my_y[mask])
+    newy_t = newy.transpose(1, 0).reshape((new_shape,) + oldy_shape)
+    return newy_t
