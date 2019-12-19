1112824cbafceaedc0b1af36610189f795d63850
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2019-12-19 11:35:34 +0000

    Refactor test_s2_obs and update test references.

--- kaska/tests/test_s2_obs.py
+++ kaska/tests/test_s2_obs.py
@@ -18,6 +18,22 @@ TEST_PATH = Path(os.path.dirname(__file__))
 def test_s2_data():
     """Test the reading of s2 time series data.
     """
+    output_names = ['rho_surface', 'mask', 'sza', 'vza', 'raa', 'rho_unc']
+
+    def test_missing_file_in_dict(date, pop_index):
+        popped_file = s2_obs.date_data[date].pop(pop_index)
+        output_data = dict(zip(output_names, s2_obs.read_granule(date)))
+        assert list(output_data.values()) == [None] * len(output_names)
+        s2_obs.date_data[date] = list(file_list_orig)
+
+        return popped_file
+
+    def test_missing_file(filepath, date):
+        shutil.move(filepath, filepath.with_suffix(".bac"))
+        output_data = dict(zip(output_names, s2_obs.read_granule(date)))
+        assert list(output_data.values()) == [None] * len(output_names)
+        shutil.move(filepath.with_suffix(".bac"), filepath)
+
 
     today = dt.datetime(2017, 1, 1)
     time_grid = [today + dt.timedelta(days=n) for n in range(0, 31, 5)]
@@ -46,63 +62,39 @@ def test_s2_data():
     assert list(s2_obs.date_data) == ref_dates
     assert [obs[0].parent for _, obs in s2_obs.date_data.items()] == ref_files
 
+    # Test read_granule for missing cloud mask file
+    test_missing_file(ref_files[1].parent / f"cloud.tif", ref_dates[1])
+
+    # Test read_granule for reflectivity file missing in the dictionary
+    file_list_orig = list(s2_obs.date_data[ref_dates[1]])
+    file_miss = test_missing_file_in_dict(ref_dates[1], 0)
+    # Test read_granule for reflectivity file missing in the folder
+    test_missing_file(file_miss, ref_dates[1])
+
+    # Test read_granule for reflectivity uncertainty file missing in the
+    # dictionary
+    file_miss = test_missing_file_in_dict(ref_dates[1], 1)
+    # Test read_granule for reflectivity uncertainty file missing in the folder
+    test_missing_file(file_miss, ref_dates[1])
+
+    # Test read_granule for sun angle file missing
+    test_missing_file(ref_files[1].parent / "ANG_DATA" / "SAA_SZA.tif",
+                      ref_dates[1])
+    # Test read_granule for view angle file missing
+    test_missing_file(ref_files[1].parent / "ANG_DATA" / "VAA_VZA_B05.tif",
+                      ref_dates[1])
+
     # Test read_granule when all data files are available
-    output_names = ['rho_surface', 'mask', 'sza', 'vza', 'raa', 'rho_unc']
     for i, d in enumerate(ref_dates):
         output_data = dict(zip(output_names, s2_obs.read_granule(d)))
-        # np.savez_compressed(f"s2_obs_{i}_ref", rho_surface=rho_surface,
-        #                    mask=mask, sza=sza, vza=vza, raa=raa,
-        #                    rho_unc=rho_unc)
+        # np.savez_compressed(f"s2_obs_{i}_ref",
+        #                    rho_surface=output_data['rho_surface'],
+        #                    mask=output_data['mask'], sza=output_data['sza'],
+        #                    vza=output_data['vza'], raa=output_data['raa'],
+        #                    rho_unc=output_data['rho_unc'])
         ref_data = np.load(parent / 'data' / f's2_obs_{i}_ref.npz',
                            allow_pickle=True)
         for datum in output_names:
             np.testing.assert_equal(ref_data[datum], output_data[datum])
-
-    # Test read_granule for missing cloud mask file
-    shutil.move(ref_files[1].parent / f"cloud.tif",
-                ref_files[1].parent / f"cloud.tif.bac")
-    output_data = dict(zip(output_names, s2_obs.read_granule(ref_dates[1])))
-    assert list(output_data.values()) == [None] * len(output_names)
-    shutil.move(ref_files[1].parent / f"cloud.tif.bac",
-                ref_files[1].parent / f"cloud.tif")
-
-    # Test read_granule for reflectivity file missing in the dictionary
-    file_list = list(s2_obs.date_data[ref_dates[1]])
-    file0 = s2_obs.date_data[ref_dates[1]].pop(0)
-    output_data = dict(zip(output_names, s2_obs.read_granule(ref_dates[1])))
-    assert list(output_data.values()) == [None] * len(output_names)
-    s2_obs.date_data[ref_dates[1]] = list(file_list)
-    # Test read_granule for reflectivity file missing in the folder
-    shutil.move(file0, file0.with_suffix(".bac"))
-    output_data = dict(zip(output_names, s2_obs.read_granule(ref_dates[1])))
-    assert list(output_data.values()) == [None] * len(output_names)
-    shutil.move(file0.with_suffix(".bac"), file0)
-
-    # Test read_granule for reflectivity uncertainty file missing in the
-    # dictionary
-    file1 = s2_obs.date_data[ref_dates[1]].pop(1)
-    output_data = dict(zip(output_names, s2_obs.read_granule(ref_dates[1])))
-    assert list(output_data.values()) == [None] * len(output_names)
-    s2_obs.date_data[ref_dates[1]] = list(file_list)
-    # Test read_granule for reflectivity uncertainty file missing in the folder
-    shutil.move(file1, file1.with_suffix(".bac"))
-    output_data = dict(zip(output_names, s2_obs.read_granule(ref_dates[1])))
-    assert list(output_data.values()) == [None] * len(output_names)
-    shutil.move(file1.with_suffix(".bac"), file1)
-
-    # Test read_granule for sun angle file missing
-    shutil.move(ref_files[1].parent / "ANG_DATA" / "SAA_SZA.tif",
-                ref_files[1].parent / "ANG_DATA" / "SAA_SZA.tif.bac")
-    output_data = dict(zip(output_names, s2_obs.read_granule(ref_dates[1])))
-    assert list(output_data.values()) == [None] * len(output_names)
-    shutil.move(ref_files[1].parent / "ANG_DATA" / "SAA_SZA.tif.bac",
-                ref_files[1].parent / "ANG_DATA" / "SAA_SZA.tif")
-    # Test read_granule for view angle file missing
-    shutil.move(ref_files[1].parent / "ANG_DATA" / "VAA_VZA_B05.tif",
-                ref_files[1].parent / "ANG_DATA" / "VAA_VZA_B05.tif.bac")
-    output_data = dict(zip(output_names, s2_obs.read_granule(ref_dates[1])))
-    assert list(output_data.values()) == [None] * len(output_names)
-    shutil.move(ref_files[1].parent / "ANG_DATA" / "VAA_VZA_B05.tif.bac",
-                ref_files[1].parent / "ANG_DATA" / "VAA_VZA_B05.tif")
 
     # assert False
